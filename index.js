14efa0303a61948d85ec76bee634f321c58a1cc5
thana.wisutphusi <thana.wisutphusi@thoughtworks.com>
2022-08-31 19:44:53 +0700

    remove hello

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ app.get("/", (req, res) => {
   res.json({ message: "hi ha" });
 });
 
-app.get("/hello", (req, res) => {
-  res.json({ message: "hello world" });
-});
-
 app.listen(3000, () => {
   console.log("app is up and running on port 3000");
 });
